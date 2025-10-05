71af02a1c640edfb2b30d990b5e70e040cce73b0
shanooo773 <shanooo773@gmail.com>
2025-10-06 01:21:49 +0500

    final

---
+++ projects/13/welcome.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Welcome to your Robot Console workspace!
+
+This is your personal development environment for robot programming.
+Your work is automatically saved and will persist between sessions.
+"""
+
+def main():
+    print("Welcome to Robot Console! this test python file for test user")
+    print("Your development environment is ready.")
+    
+    # Example robot code snippet
+    robot_position = {"x": 0, "y": 0, "angle": 0}
+    print(f"Robot position: {robot_position}")
+    
+    # TODO: Add your robot control code here
+
+if __name__ == "__main__":
+    main()
+   
---
+++ projects/4/robot_example.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+class Robot {
+public:
+    double x, y, angle;
+    
+    Robot(double x = 0, double y = 0, double angle = 0) 
+        : x(x), y(y), angle(angle) {}
+    
+    void move(double distance) {
+        x += distance * std::cos(angle);
+        y += distance * std::sin(angle);
+        std::cout << "Robot moved to (" << x << ", " << y << ")" << std::endl;
+    }
+    
+    void rotate(double angle_change) {
+        angle += angle_change;
+        std::cout << "Robot rotated to " << angle << " radians" << std::endl;
+    }
+};
+
+int main() {
+    std::cout << "My name is shayan" << std::endl;
+    Robot robot(0, 0, 0);
+    return 0;
+}
